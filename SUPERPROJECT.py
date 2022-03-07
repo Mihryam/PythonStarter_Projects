d968cc307697f2ccae572fd46b735c194bd9320b
Mihryam <janeamaefule1@gmail.com>
2022-03-07 16:56:37 +0100

    Pystarter_Project

--- SUPERPROJECT.py
+++ SUPERPROJECT.py
@@ -8,28 +8,38 @@
 # except ZeroDivisionError:
 #     print('Cant do this')    
 
+def tester():
+    try:
+        bestieNum = int(input('Please input your bestie number:  '))
+        specialNum = int(input('Special number please: '))
+        average_nom = bestieNum/specialNum
 
-try:
-    bestieNum = int(input('Please input your bestie number:  '))
-    bestie = input('What is your bestfriend name: ')
-    specialNum = int(input('Special number please: '))
-    average_nom = bestieNum/specialNum
-      
+        def bestieName():
+
+            bestie = input('What is your bestfriend name: ')
+            if bestie.isdigit():
+                print('Your bestfriend is a number?!! Really?')
+                bestieName()
+
+            elif bestie.isalpha():
+                print(f'My best friend name is {bestie.title()}. This is our mutual average no : {average_nom}')  
+            else:
+                print('Please input yur best frirnd name correctly')
+                bestieName()
+        bestieName()        
+
+
     
-    if bestie.isdigit():
-        print('Your bestfriend is a number?!! Really?')
-    elif bestie.isalpha():
-        print(f'My best friend name is {bestie.title()}. This is our mutual average no : {average_nom}')  (
-    
-    print('Yoy dont want your betie to know that you cant spell. Do you?')          
-    
-except ValueError:
-    print('Incorrect Value!!! Plese input the right value')
-except ZeroDivisionError:
-    print('Please check your values, you have a scary number 0: ')
+    except ValueError:
+        print('Incorrect Value!!! Plese input the right value')
+        tester()
 
 
+    except ZeroDivisionError:
+        print('Please check your values, you have a scary number 0: ')
 
-a
+tester()
+
+
     
             
